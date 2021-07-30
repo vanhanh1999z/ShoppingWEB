ab209333aa01623f7e5e30d558cfb63496a573d3
vanhanh1999z <48524076+vanhanh1999z@users.noreply.github.com>
2021-07-30 20:43:34 +0700

    complete 30/7

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,53 @@
 <template>
   <div class="app">
-    <header>
-      <Nav />
-    </header>
-    <br>
-    <br>
-    <div class="auth-wrapper">
-        <router-view />
-    </div>
+    <b-container fluid="md">
+      <b-row>
+        <b-col style="padding:0;">
+          <header>
+            <Nav />
+          </header>
+        </b-col>
+      </b-row>
+      <b-row style="margin-top:60px;">
+        <b-col sm="3" class="justify-content-start">
+          <div id="MenuLeft">
+            <Menuleft />
+          </div>
+          <!-- <Category>  
+          </Category> -->
+        </b-col>
+        <b-col sm="9">
+          <div>
+            <router-view />
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import Nav from './components/Nav'
-import axios from 'axios'
+import Menuleft from "./components/MenuLeft.vue";
+import Nav from "./components/Nav";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
-    return{
-      user: null
-    }
+    return {
+      user: null,
+    };
   },
   components: {
     Nav,
+    Menuleft,
   },
-  
-  async created(){
-    const response = await axios.get('user');
 
-    this.$store.dispatch('user', response.data);
-  }
+  async created() {
+    const response = await axios.get("user");
+
+    this.$store.dispatch("user", response.data);
+  },
 };
 </script>
 
@@ -52,73 +69,79 @@ export default {
   margin-top: 10px;
 }
 
-body{
-  background: #1C8EF9 !important;
+body {
+  background: #1c8ef9 !important;
   min-height: 100vh;
   display: flex;
   font-weight: 400;
-  font-family: 'Fira San', sans-serif; 
+  font-family: "Fira San", sans-serif;
 }
 
-
-h1, h2, h3, h4, h5, h6, label, span{
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+label,
+span {
   font-weight: 500;
-  font-family: 'Fira San', sans-serif;
+  font-family: "Fira San", sans-serif;
 }
 
-
-body, htmk, #app, #root, .auth-wrapper{
+body,
+htmk,
+#app,
+#root,
+.auth-wrapper {
   width: 100%;
   height: 100%;
 }
 
-#app{
+#app {
   text-align: center;
 }
 
-.navbar-light{
+.navbar-light {
   background-color: #ffffff;
   box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
 }
 
-.auth-wrapper{
+.auth-wrapper {
   display: flex;
   justify-content: center;
   flex-direction: column;
   text-align: left;
 }
 
-
-.auth-inner{
+.auth-inner {
   width: 450px;
   margin: auto;
   background: #ffffff;
   box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
   padding: 40px 55px 45px 55px;
   border-radius: 15px;
-  transition: all .3s;
+  transition: all 0.3s;
 }
 
-
-.auth-wrapper .form-control:focus{
+.auth-wrapper .form-control:focus {
   border-color: #167bff;
   box-shadow: none;
 }
 
-.auth-wrapper h3{
+.auth-wrapper h3 {
   text-align: center;
   margin: 0;
   line-height: 1;
   padding-bottom: 20px;
 }
 
-
-.custom-control-lable{
+.custom-control-lable {
   font-weight: 400;
 }
 
 .forgot-password,
-.forgot-password a{
+.forgot-password a {
   text-align: right;
   font-size: 13px;
   padding-top: 10px;
@@ -126,8 +149,7 @@ body, htmk, #app, #root, .auth-wrapper{
   margin: 0;
 }
 
-
-.forgot-password a{
+.forgot-password a {
   color: #167bff;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import shoppingCart from '../components/shoppingCart.vue'
 import Main from '../components/Main.vue'
 import Checkout from '../components/Checkout.vue'
-import Home from '../components/Home.vue'
+
 import Login from '../components/Login.vue'
 import Register from '../components/Register.vue'
 import Forgot from '../components/Forgot.vue'
@@ -14,7 +14,7 @@ const routes = [
   {path: '/cart', component: shoppingCart},
   {path: '/main', component: Main},
   {path:'/check-out', component: Checkout},
-  {path:'/', component: Home},
+  {path:'/', component: Main},
   {path:'/login', component: Login},
   {path:'/register', component: Register},
   {path:'/forgot', component: Forgot}
